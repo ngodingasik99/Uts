e7ee4f42b0d6b6fd20bc78cc90770f369b220e53
ngodingasik99 <ngodingasik7@gmail.com>
2023-04-09 21:41:39 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout/admin');
 });
+//produk
+
+
+
+
+
+
+
+
+
+//kategori
+
+
+
+
+
+
+
+
+
+//cart
+
+
+
+
+
+
+
+
+
+
+//transaksi
