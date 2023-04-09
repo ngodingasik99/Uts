87f852971242c7ec2fbdd0a4d698884f136ff4d2
aldhitadwilestari <aldhitadwilestari@gmail.com>
2023-04-09 21:54:25 +0700

    commit kelima

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/layout.admin', function () {
     return view('welcome');
 });
+
+//product
+Route::get('/product', [ProductController::class, 'index'])->middleware('auth:web');
+
+Route::get('product.add', [ProductController::class, 'create']);
+
+Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
+
+Route::get('/product/{id}/delete', [ProductController::class, 'destroy']);
+
+Route::post('/product', [ProductController::class, 'store' ]);
+
+Route::put('/product/{id}', [ProductController::class, 'update']);
+
+//category
+Route::get('/category', [CategoryController::class, 'index'])->middleware('auth:web');
+
+Route::get('category.add', [CategoryController::class, 'create']);
+
+Route::get('/category/{id}/edit', [CategoryController::class, 'edit']);
+
+Route::get('/category/{id}/delete', [CategoryController::class, 'destroy']);
+
+Route::post('/category', [CategoryController::class, 'store' ]);
+
+Route::put('/category/{id}', [CategoryController::class, 'update']);
+
+
