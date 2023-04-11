7b8f5c9a2b3fbaade5cc109922fb819de107988c
ngodingasik99 <ngodingasik7@gmail.com>
2023-04-11 12:07:04 +0700

    perbaikan bentrok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
+use App\Models\User; 
 use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
