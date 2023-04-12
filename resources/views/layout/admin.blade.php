d9a2b756360234a109b818e3042a07b1b25c8a5a
ngodingasik99 <ngodingasik7@gmail.com>
2023-04-12 10:29:38 +0700

    commit: add script layout

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -17,11 +17,6 @@
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
-  <!-- Preloader -->
-  {{-- <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="{{ asset('template/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
-  </div> --}}
-
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-dark">
     <!-- Left navbar links -->
@@ -33,6 +28,13 @@
         <a href="index3.html" class="nav-link">Home</a>
       </li>
     </ul>
+
+  <style>
+    .geser{
+      margin-left: 65%;
+    }
+  </style>
+      <span class="geser"><i class="fas fa-shopping-cart"></i></span>
 
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
@@ -47,7 +49,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
       <img src="{{ asset('template/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Baju distro</span>
+      <span class="brand-text font-weight-light">clothes cashier</span>
     </a>
 
     <!-- Sidebar -->
@@ -94,7 +96,7 @@
   <aside class="control-sidebar control-sidebar-dark">
   </aside>
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">Kelompok 12</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
